da64efd295898104ff01a9088598454e5551b366
FarmerRobbie <farmerrobbie@freshsalad.today>
2015-02-10 04:15:34 -0800

    added EC Rectifier Arduino sketch

--- Arduino Sketches/EC_Rectifier_Test/EC_Rectifier_Test.ino
+++ Arduino Sketches/EC_Rectifier_Test/EC_Rectifier_Test.ino
@@ -1,21 +1,26 @@
 /*
 Copyright Margaret Johnson 1/2015
-*/
+ */
 #include <Statistic.h>
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
 Adafruit_ADS1015 ads1015;
-const byte gate_pin = 8;
-const byte ain_pin = 7;
+const byte FET_pin = 8;
+const byte mux_out_pin = 7;
+const byte mux_enable_pin = 6;
 const byte LSB_multiplier = 1;// 2x gain   +/- 2.048V  1 bit = 1mV
 Statistic myStats_Vin;  //from http://playground.arduino.cc/Main/Statistics
 Statistic myStats_ECv;
 void setup(void)
 {
   Serial.begin(9600);
-  pinMode(gate_pin,OUTPUT);
-  pinMode(ain_pin,OUTPUT);
+  pinMode(FET_pin,OUTPUT);
+  pinMode(mux_out_pin,OUTPUT);
+  pinMode(mux_enable_pin,OUTPUT); 
+  digitalWrite(FET_pin,LOW); //start with FET as open circuit
+  digitalWrite(mux_enable_pin,HIGH); //disable multiplexer
+
   ads1015.begin();
   //Vpp a bit over 2V
   // ads1015.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 2mV
@@ -34,11 +39,16 @@ void serialHandler() {
   char inChar;
   if((inChar = Serial.read())>0) {
     switch (inChar) {
-    case 'r': // take reading
-    case 'R':
-      readEC();
+    case 'i': // take ECVin+ reading
+    case 'I':
+      readECVin();
       showHelp();
       break;
+    case 'o': // take ECVout reading
+    case 'O':
+      readECVout();
+      showHelp();
+      break;      
     case '?':
     case 'h': // Display help
       showHelp();
@@ -50,11 +60,11 @@ void serialHandler() {
 }
 #define VIN  1
 #define ECV  2
-void readEC()
+
+void readECVin()
 {
-  Serial.println("readEC");
+  Serial.println("readECVin");
   myStats_Vin.clear(); //explicitly start clean
-  myStats_ECv.clear();
   Serial.println(F("Enter number readings: "));
   while (Serial.available () == 0) {
     ;
@@ -62,75 +72,88 @@ void readEC()
   unsigned int num_readings = Serial.parseInt();
   Serial.println(num_readings);
   int vIn_value = readVIn(num_readings);
-  Serial.print("Vin+: ");
-  Serial.println(vIn_value);
-  int vEC_value = readECv(num_readings);
   Serial.print(F("|  Count: "));
   Serial.print(myStats_Vin.count());
   Serial.print(F("**  Vin+ ** Average: "));
   Serial.print(myStats_Vin.average());
   Serial.print(F("|  Std deviation: "));
-  Serial.print(myStats_Vin.pop_stdev());
-  Serial.print(F("**  ECv ** Average: "));
+  Serial.println(myStats_Vin.pop_stdev());
+}
+void readECVout()
+{
+  Serial.println("readECVout");
+  myStats_ECv.clear(); //explicitly start clean
+  Serial.println(F("Enter number readings: "));
+  while (Serial.available () == 0) {
+    ;
+  }
+  unsigned int num_readings = Serial.parseInt();
+  Serial.println(num_readings);
+  int vIn_value = readECv(num_readings);
+  Serial.print(F("|  Count: "));
+  Serial.print(myStats_ECv.count());
+  Serial.print(F("**  ECVout ** Average: "));
   Serial.print(myStats_ECv.average());
   Serial.print(F("|  Std deviation: "));
   Serial.println(myStats_ECv.pop_stdev());
-  float gain = (float)vEC_value/(float)vIn_value;   // G = Vout/Vin
-  float rEC = (float)1000/(gain-1.);
-  Serial.print(F("** Gain: "));
-  Serial.print(gain);
-  Serial.print(F("| ReC: "));
-  Serial.print(rEC);
-  Serial.print(F("|EC: "));
-  float mS = 1/rEC*1000;
-  Serial.print(mS);
-  Serial.println("mS");
+
 }
 int readVIn(unsigned int num_readings)
 {
-  dischargeCapacitor();
+  digitalWrite(FET_pin,LOW); //stop draining
+  digitalWrite(mux_enable_pin,LOW);  //enable multiplexer
   switchTo(VIN);
   for (int i=0;i<num_readings;i++) {
     int results = readADC();
     myStats_Vin.add(results);
   }
+  digitalWrite(mux_enable_pin,HIGH); //disable multiplexer
+  //  digitalWrite(FET_pin,HIGH);  //start draining
   return myStats_Vin.average();
 }
 int readECv(unsigned int num_readings)
 {
-  dischargeCapacitor();
+  digitalWrite(FET_pin,LOW);  //stop draining
+  digitalWrite(mux_enable_pin,LOW); //enable multiplexer
   switchTo(ECV);
   for (int i=0;i<num_readings;i++) {
     int results = readADC();
     myStats_ECv.add(results);
   }
+  digitalWrite(mux_enable_pin,HIGH); //disable multiplexer
+  //  digitalWrite(FET_pin,HIGH);  //start draining
   return myStats_ECv.average();
 }
 int adcReading(unsigned int num_readings)
 {
+  discharge(); //discharge capacitor before taking readings
   for (int i=0;i<num_readings;i++) {
     int results = readADC();
+    discharge(); //discharge after a reading
     myStats_Vin.add(results);
   }
   return myStats_Vin.average();
 }
-void dischargeCapacitor()
-{
-  digitalWrite(gate_pin,HIGH); //Discharge the capacitor
-  digitalWrite(gate_pin,LOW);  //Open the short circuit
-}
+
 void switchTo(const byte waveform)
 {
+  Serial.println("Switch");
   if (waveform == VIN) {
     Serial.println("Reading Vin+");
-    digitalWrite(ain_pin,HIGH); //See the EC schematic
-    delay(1000);
+    digitalWrite(mux_out_pin,LOW);  //ECVin+ when pin is low.
   }
   else {
     Serial.println("Reading ECv");
-    digitalWrite(ain_pin,LOW); //default to ECv
-    delay(1000);
+    digitalWrite(mux_out_pin,HIGH); //ECVout when pin is high.
   }
+  Serial.println("Settle...");
+  delay(1000);
+}
+void discharge()
+{
+  digitalWrite(FET_pin,HIGH);
+  //delay(5000);
+  digitalWrite(FET_pin,LOW);
 }
 int16_t readADC()
 {
@@ -142,7 +165,8 @@ const char helpText[] PROGMEM =
 "\n"
 "Available commands:" "\n"
 "  ?     - shows available comands" "\n"
-"  r     - take reading" "\n"
+"  i     - take ECVin+ reading" "\n"
+"  o     - take ECVout reading" "\n"
 ;
 /*-----------------------------------------------------------
  show command line menu
@@ -160,6 +184,9 @@ static void showString (PGM_P s) {
     Serial.print(c);
   }
 }
+
+
+
 
 
 
